6471dcfa39ddf3a49714b562cde9843e40676d45
Ajaypal66626 <ajaypalsinghgour@gmail.com>
2026-08-02 03:47:24 +0530

    Small Changes

--- tests/Locator_css_selector.spec.ts
+++ tests/Locator_css_selector.spec.ts
@@ -5,10 +5,8 @@ test('Locator Css selector', async({page})=>
 
     await page.goto('https://tutorialsninja.com/demo/index.php?route=product/category&path=18');
 
-    await page.locator('input#input-firstname').fill('Ajay'); //Locator By ID: tagname#id_name or #id_name
+    await page.locator('input#input-firstname').fill('Ajay pal'); //Locator By ID: tagname#id_name or #id_name
     await page.locator('input[name="lastname"]').fill('Pal'); // Locator By Value : tagname[Value="name"] 
     await page.locator('input.btn').click(); // Locator By Class : tagname.class_name or .class_name
 
-
-    
 });
