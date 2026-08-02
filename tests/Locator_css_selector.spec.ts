16a5944610af56503093939dfff419ea201efa7e
Ajaypal66626 <ajaypalsinghgour@gmail.com>
2026-08-02 14:26:32 +0530

    Try with Git UI

--- tests/Locator_css_selector.spec.ts
+++ tests/Locator_css_selector.spec.ts
@@ -6,8 +6,8 @@ test('Locator Css selector', async({page})=>
     await page.goto('https://tutorialsninja.com/demo/index.php?route=account/register');
     
 
-    await page.locator('#input-firstname').fill('Ajay pal'); //Locator By ID: tagname#id_name or #id_name
-    await page.locator('input[name="lastname"]').fill('Pal'); // Locator By Value : tagname[Value="name"] 
+    await page.locator('#input-firstname').fill('Ajay Pal'); //Locator By ID: tagname#id_name or #id_name
+    await page.locator('input[name="lastname"]').fill('Singh Gour'); // Locator By Value : tagname[Value="name"] 
     await page.locator('input.btn').click(); // Locator By Class : tagname.class_name or .class_name
 
 });
